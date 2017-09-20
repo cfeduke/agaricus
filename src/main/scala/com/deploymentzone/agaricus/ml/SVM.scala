ea58eae08b0bb29cf697a7b37582cf33651f5ad0
Charles Feduke <charles.feduke@gmail.com>
2017-09-19 21:37:00 -0400

    conversion of categories to SVM format for labelled point

--- src/main/scala/com/deploymentzone/agaricus/ml/SVM.scala
+++ src/main/scala/com/deploymentzone/agaricus/ml/SVM.scala
@@ -1,5 +1,24 @@
 package com.deploymentzone.agaricus.ml
 
-case class SVM() {
+import com.deploymentzone.agaricus.io.Attribute
+import org.apache.spark.mllib.linalg
+import org.apache.spark.mllib.linalg.Vectors
 
+class SVM private(categories: Seq[Map[String, Double]]) {
+  def toVector(values: Array[String]): linalg.Vector = {
+    require(values.length == categories.length)
+
+    Vectors.dense(values.zip(categories).map { case (key, map) =>
+        map.getOrElse(key, 0.5) // 0.5 is used for non-explicit ? [missing] values
+    })
+  }
+}
+
+object SVM {
+  def apply(attributes: Seq[Attribute]): SVM = {
+    val categories = attributes.map { attr =>
+      attr.descriptions.zipWithIndex.map { case (d, idx) => (d.letter, idx + 1.0) }.toMap
+    }
+    new SVM(categories)
+  }
 }
