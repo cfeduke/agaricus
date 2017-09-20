0d09066692ebf9f881ac66e17f6fc259cb1b0273
Charles Feduke <charles.feduke@gmail.com>
2017-09-19 22:28:13 -0400

    training a model to around 94% accuracy with logistic regression

--- src/main/scala/com/deploymentzone/agaricus/Agaricus.scala
+++ src/main/scala/com/deploymentzone/agaricus/Agaricus.scala
@@ -2,14 +2,15 @@ package com.deploymentzone.agaricus
 
 import com.deploymentzone.agaricus.io.{Attributes, Data}
 import com.deploymentzone.agaricus.ml.SVM
-import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.SparkSession
 
 /**
   * Agaricus Lepiota driver.
   */
 object Agaricus {
-  def main(args: Array[String]): Unit = {
+  def apply(): LogisticRegressionModel = {
 
     val attributes = Attributes()
     val svm = SVM.initialize(attributes)
@@ -22,10 +23,17 @@ object Agaricus {
     val Array(training, test) = data.randomSplit(Array(8.0, 2.0))
 
     val lr = new LogisticRegression()
-        .setMaxIter(100)
+        .setMaxIter(10)
 
-    val model = lr.fit(training.toDF)
+    val model: LogisticRegressionModel = lr.fit(training.toDF)
 
-    model.evaluate(test.toDF).predictions.show(20)
+    val predictions = model.evaluate(test.toDF).predictions
+    val metrics = new MulticlassMetrics(predictions.select($"prediction", $"label").map {
+      row => (row.getDouble(0), row.getDouble(1))
+    }.rdd)
+
+    print(f"Model Accuracy: ${metrics.accuracy}%2.2f")
+
+    model
   }
 }
